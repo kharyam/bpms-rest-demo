423c268b814fe59d8e03e42ac85a28377249ce48
Khary <kharyam@gmail.com>
2015-11-09 09:00:14 -0500

    Cleanup

--- insurancerest/src/main/java/com/example/BpmsJavaRemoteExample.java
+++ insurancerest/src/main/java/com/example/BpmsJavaRemoteExample.java
@@ -16,6 +16,10 @@ import org.kie.api.task.TaskService;
 import org.kie.api.task.model.TaskSummary;
 import org.kie.services.client.api.RemoteRuntimeEngineFactory;
 
+/**
+ * A class demonstrating the use of the BPMS Remote Jav (REST) API
+ *
+ */
 public class BpmsJavaRemoteExample {
 
   private static final String DEPLOYMENT_ID = "com.example:insurance:1.0";
@@ -27,36 +31,63 @@ public class BpmsJavaRemoteExample {
   private AuditService auditService;
   private TaskService taskService;
 
-  public long startProcess() {
+  /**
+   * Default constructor, use predefined parameters
+   */
+  public BpmsJavaRemoteExample() {
+    this(DEPLOYMENT_ID, APP_URL, USER, PASSWORD);
+  }
 
+  /**
+   * Creates the runtime engine and services
+   * 
+   * @param deploymentId
+   * @param appUrl
+   * @param user
+   * @param password
+   */
+  public BpmsJavaRemoteExample(String deploymentId, String appUrl, String user, String password) {
     URL url = null;
 
     try {
-      url = new URL(APP_URL);
+      url = new URL(appUrl);
     } catch (MalformedURLException e) {
       e.printStackTrace();
     }
 
-    RuntimeEngine factory = RemoteRuntimeEngineFactory.newRestBuilder().addUrl(url).addUserName(USER)
-        .addPassword(PASSWORD).addDeploymentId(DEPLOYMENT_ID).build();
+    RuntimeEngine factory = RemoteRuntimeEngineFactory.newRestBuilder().addUrl(url).addUserName(user)
+        .addPassword(password).addDeploymentId(deploymentId).build();
 
     ksession = factory.getKieSession();
     taskService = factory.getTaskService();
     auditService = factory.getAuditService();
+  }
 
+  /**
+   * Start an example business process
+   * 
+   * @return process id
+   */
+  public long startProcess() {
     Map<String, Object> variables = new HashMap<String, Object>();
     variables.put("pv_firstName", "Joe");
     variables.put("pv_lastName", "Smith");
     variables.put("pv_age", new Integer(47));
     variables.put("pv_income", new Integer(100000));
-    ProcessInstance pi = ksession.startProcess("insurance.mainProcess", variables);
-    System.out.println("Created process id " + pi.getId() + "\n");
-    return pi.getId();
+
+    ProcessInstance processInstance = ksession.startProcess("insurance.mainProcess", variables);
+    System.out.println("Created process id " + processInstance.getId() + "\n");
+    return processInstance.getId();
   }
 
+  /**
+   * Exercise some of the API functionality
+   * 
+   * @param args
+   */
   public static void main(String args[]) {
     BpmsJavaRemoteExample example = new BpmsJavaRemoteExample();
-    long pid = example.startProcess();
+    long processId = example.startProcess();
     List<TaskSummary> tasks = example.taskService.getTasksAssignedAsPotentialOwner(USER, "EN_UK");
 
     System.out.println("Claimable processes:");
@@ -65,18 +96,19 @@ public class BpmsJavaRemoteExample {
       Map<String, Object> content = example.taskService.getTaskContent(summary.getId());
       Set<String> keys = content.keySet();
       for (String key : keys) {
-        System.out.println("\t"+key + " => " + content.get(key));
+        System.out.println("\t" + key + " => " + content.get(key));
       }
     }
-    
-    example.ksession.signalEvent("Task A",null, pid);
 
-    List<? extends NodeInstanceLog> completedNodes = example.auditService.findNodeInstances(pid);
-    
+    example.ksession.signalEvent("Task A", null, processId);
+
+    List<? extends NodeInstanceLog> completedNodes = example.auditService.findNodeInstances(processId);
+
     System.out.println("\nStarted Nodes:");
     for (NodeInstanceLog node : completedNodes) {
-      System.out.println(node.getNodeId() + " - " + node.getNodeName() + " - " + node.getNodeType() + " - " + node.getType());
+      System.out.println(node.getNodeId() + " - " + node.getNodeName() + " - " + node.getNodeType() + " - "
+          + node.getType());
     }
-    
+
   }
 }
