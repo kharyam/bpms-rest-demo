7a539300e3a2f842edf5cffa2f95dbd5b70b51d7
Khary <kharyam@gmail.com>
2015-11-08 19:42:30 -0500

    Cleaned up code (authentication, claim /start logic)

--- insuranceui/src/main/webapp/actions.js
+++ insuranceui/src/main/webapp/actions.js
@@ -1,32 +1,29 @@
-var USER = "bpmsAdmin";
-var PASSWORD = "p@ssw0rd";
-var url = "http://" + USER + ":" + PASSWORD + "@localhost:8080/business-central";
-
 // Register UI callbacks
 $(document).ready(function() {
-  
-  $("#setCredentials").click(showApplication);
-  
-	$("#getApplications").click(getApplications);
 
-	$('#taskListTable').bootstrapTable({}).on('click-row.bs.table', function (e, row, $element) {
-		startReview(row);});	
-	
-	$("#apply").click(apply);
-	
-	$("#taskA").click(adHocA);
-	
-	$("#taskB").click(adHocB);
-	
-	$("#save").click(saveReview);
-	
-	$("#complete").click(completeReview);
-	
+  $("#setCredentials").click(showApplication);
+
+  $("#getApplications").click(getApplications);
+
+  $('#taskListTable').bootstrapTable({}).on('click-row.bs.table', function(e, row, $element) {
+    startReview(row);
+  });
+
+  $("#apply").click(apply);
+
+  $("#taskA").click(adHocA);
+
+  $("#taskB").click(adHocB);
+
+  $("#save").click(saveReview);
+
+  $("#complete").click(completeReview);
+
 });
 
 
 function getUrl(){
-  return "http://" + $('#userName').val() + ":" + $('#password').val() + "@localhost:8080/business-central";
+  return "http://localhost:8080/business-central";
 }
 
 function showApplication() {
@@ -37,16 +34,20 @@ function showApplication() {
   }
 }
 
-function apply(){  
+function apply() {
   $.ajax({
-      type: "POST",
-      url: getUrl() + "/rest/runtime/com.example:insurance:1.0/process/insurance.mainProcess/start",
+    type : "POST",
+    url : getUrl() + "/rest/runtime/com.example:insurance:1.0/process/insurance.mainProcess/start",
 
       data: "map_pv_firstName=" + $('#firstName').val() + 
             "&map_pv_lastName=" + $('#lastName').val() + 
             "&map_pv_age=" + $('#age').val() + "i" +
             "&map_pv_income=" + $('#income').val() + "i",
 
+      headers: {
+                "Authorization": "Basic " + btoa($('#userName').val() + ":" + $('#password').val())
+              },
+            
       success : function (data,status) {
               $('#applicationForm').hide(500);
               $('#thankYouForm').show(500);
@@ -55,7 +56,7 @@ function apply(){
       error : function (data,status) {
               alert("Something went wrong, try again later. (status = " + status);
             }
-            
+      
   });
   
 }
@@ -65,7 +66,14 @@ function getApplications() {
 }
 
 function getTaskList(user){
-	$.getJSON(getUrl() + "/rest/task/query?potentialOwner="+user, taskListCB);
+    $.ajax({
+	    type: "GET",
+	    dataType: "json",
+	    url: getUrl() + "/rest/task/query?potentialOwner="+user,
+	    headers: {"Authorization": "Basic " + btoa($('#userName').val() + ":" + $('#password').val())},
+	    success : taskListCB,
+	    error : function (data,status) {alert("Something went wrong, try again later. (status = " + status);}
+    	});
 }
 
 function taskListCB(taskList) {
@@ -91,17 +99,49 @@ function startReview(row) {
 	$('#processInstanceId').val(row.processInstanceId);
 	$('#taskId').val(row.id);
 	
-	// Claim the task  
-	$.post(getUrl() + "/rest/task/" + row.id + "/claim","",function(){console.log("Successfully claimed task");});
-	    
-  // Start the task
-	$.post(getUrl() + "/rest/task/" + row.id + "/start","",function(){console.log("Successfully started task");});
+	if (row.status == "Ready") {
+		claimTask();
+	}
 	
-	// Get the variable values
-	$.getJSON(getUrl() + "/rest/query/runtime/task?taskid=" + row.id, populateReviewFormCB);
-	 
+	if (row.status == "Reserved") {
+		startTask();
+	}
+	
+	getTaskVariables();
+	
 	$('#tasksPanel').hide(250);
 	$('#reviewForm').show(250);	
+}
+
+function claimTask(){
+    $.ajax({
+	    type: "POST",
+	    url: getUrl() + "/rest/task/" + $('#taskId').val() + "/claim",
+	    headers: {"Authorization": "Basic " + btoa($('#userName').val() + ":" + $('#password').val())},
+	    success : function(){console.log("Successfully claimed task");startTask();},
+	    error : function (data,status) {console.log("Something went wrong, try again later. (status = " + status);}
+		});
+}
+
+function startTask(){
+    $.ajax({
+	    type: "POST",
+	    url: getUrl() + "/rest/task/" + $('#taskId').val() + "/start",
+	    headers: {"Authorization": "Basic " + btoa($('#userName').val() + ":" + $('#password').val())},
+	    success : function(){console.log("Successfully started task")},
+	    error : function (data,status) {console.log("Something went wrong, try again later. (status = " + status);}
+		});
+}
+
+function getTaskVariables() {
+    $.ajax({
+	    type: "GET",
+	    dataType: "json",
+	    url: getUrl() + "/rest/query/runtime/task?taskid=" + $('#taskId').val(),
+	    headers: {"Authorization": "Basic " + btoa($('#userName').val() + ":" + $('#password').val())},
+	    success : populateReviewFormCB,
+	    error : function (data,status) {alert("Something went wrong, try again later. (status = " + status);}
+    	});
 }
 
 
@@ -141,43 +181,32 @@ function completeReview() {
           "&map_out_income=" + $('#income').val() + "i" +
           "&map_out_status=" + $('#status').val(),
 
+    headers: {"Authorization": "Basic " + btoa($('#userName').val() + ":" + $('#password').val())},
+            
     success : function (data,status) {
-      
             getApplications(); // refresh application list
             $('#reviewForm').hide(500);
             $('#taskPanel').show(500);
           },
 
-    error : function (data,status) {
-            alert("Something went wrong, try again later. (status = " + status);
-          }
-          
-});
-  
+    error : function (data,status) {alert("Something went wrong, try again later. (status = " + status);}
+  });
 }
 
-
-function adHocA(){
+function adHocA() {
   signal("Task A");
 }
 
-function adHocB(){
+function adHocB() {
   signal("Task B");
 }
 
 function signal(source) {
-$.ajax({
-    type : "POST",
-    url : getUrl() + "/rest/runtime/com.example:insurance:1.0/process/instance/" + $('#processInstanceId').val() + "/signal?signal=" + source, 
-    
-    success : function (data,status) {
-      alert("Successfully signalled " + source);
-    },
-    
-    error : function (data,status) {
-      alert("Failed to signal " + source + " (status = " + status + ")");
-    }
-
-});  
-  
+	$.ajax({
+		type : "POST",
+		url : getUrl() + "/rest/runtime/com.example:insurance:1.0/process/instance/" + $('#processInstanceId').val() + "/signal?signal=" + source, 
+		headers: {"Authorization": "Basic " + btoa($('#userName').val() + ":" + $('#password').val())},
+		success : function (data,status) {alert("Successfully signalled " + source);},
+		error : function (data,status) {alert("Failed to signal " + source + " (status = " + status + ")");}
+	});  
 }
